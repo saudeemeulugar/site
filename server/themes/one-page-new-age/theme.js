6b84a5622dcb6329b8d5d495b0aba350825db47c
albertosouza <alberto.souza.99@gmail.com>
2018-05-03 15:44:53 -0300

    add gov bar

--- server/themes/one-page-new-age/theme.js
+++ server/themes/one-page-new-age/theme.js
@@ -30,16 +30,16 @@ module.exports = {
           { value: '#FFD15A' }
         ]
       },
-      gray: {
-        label: 'Cores: preto, branco e cinza',
-        stylesheet: 'files/public/style-gray.css',
-        colors: [
-          { value: '#ccc' },
-          { value: '#dfdfdf' },
-          { value: '#aaa' },
-          { value: '#000' }
-        ]
-      },
+      // gray: {
+      //   label: 'Cores: preto, branco e cinza',
+      //   stylesheet: 'files/public/style-gray.css',
+      //   colors: [
+      //     { value: '#ccc' },
+      //     { value: '#dfdfdf' },
+      //     { value: '#aaa' },
+      //     { value: '#000' }
+      //   ]
+      // },
       darkgreen: {
         label: 'Cores: verde escuro, branco e vermelho claro nos links',
         stylesheet: 'files/public/style-darkgreen.css',
@@ -50,16 +50,16 @@ module.exports = {
           { value: '#00291e' }
         ]
       },
-      lightgreen: {
-        label: 'Cores: verde claro, branco e laranja claro nos links',
-        stylesheet: 'files/public/style-lightgreen.css',
-        colors: [
-          { value: '#8aff87' },
-          { value: '#FEA313' },
-          { value: '#fff' },
-          { value: '#00291e' }
-        ]
-      },
+      // lightgreen: {
+      //   label: 'Cores: verde claro, branco e laranja claro nos links',
+      //   stylesheet: 'files/public/style-lightgreen.css',
+      //   colors: [
+      //     { value: '#8aff87' },
+      //     { value: '#FEA313' },
+      //     { value: '#fff' },
+      //     { value: '#00291e' }
+      //   ]
+      // },
       darkblue: {
         label: 'Cores: azul escuro, branco e laranja claro nos links',
         stylesheet: 'files/public/style-darkblue.css',
@@ -70,16 +70,16 @@ module.exports = {
           { value: '#FEA313' }
         ]
       },
-      lightblue: {
-        label: 'Cores: azul claro e branco',
-        stylesheet: 'files/public/style-lightblue.css',
-        colors: [
-          { value: '#71C3B7' },
-          { value: '#182c2f' },
-          { value: '#fff' },
-          { value: '#FEA313' }
-        ]
-      }
+      // lightblue: {
+      //   label: 'Cores: azul claro e branco',
+      //   stylesheet: 'files/public/style-lightblue.css',
+      //   colors: [
+      //     { value: '#71C3B7' },
+      //     { value: '#182c2f' },
+      //     { value: '#fff' },
+      //     { value: '#FEA313' }
+      //   ]
+      // }
     }
   },
 
