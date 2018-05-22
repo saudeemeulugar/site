848d3c1800cc78b428a05fd54647153da07a5452
albertosouza <alberto.souza.99@gmail.com>
2018-05-22 00:19:45 -0300

    add latest-histories widget

--- server/widgets/latest-histories/index.js
+++ server/widgets/latest-histories/index.js
@@ -3,12 +3,21 @@ module.exports = function (projectPath, Widget) {
 
   widget.viewMiddleware = function(w, req, res, next) {
     req.we.db.models.history.findAll({
-      where: { published: true },
+      where: {
+        haveImage: true,
+        published: true
+      },
       order: [['publishedAt', 'DESC']],
       limit: 9
     })
     .then( (r)=> {
       w.records = r;
+
+      w.recordsC = [];
+      w.recordsC[0] = [r[0], r[1], r[2]];
+      w.recordsC[1] = [r[3], r[4], r[5]];
+      w.recordsC[2] = [r[6], r[7], r[8]];
+
       next();
       return null;
     })
