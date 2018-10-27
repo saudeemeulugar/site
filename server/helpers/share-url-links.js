e7afc87fb55765eb324b24977945aec92fa596a4
albertosouza <alberto.souza.99@gmail.com>
2018-10-26 23:43:06 -0300

    improve share links

--- server/helpers/share-url-links.js
+++ server/helpers/share-url-links.js
@@ -17,8 +17,10 @@ module.exports = function(we) {
     let title = options.hash.title;
     // let teaser = options.hash.teaser;
 
+    const siteName = (we.systemSettings.siteName || we.config.appName);
+
     let html = `
-      <a href="https://twitter.com/intent/tweet?url=${url}" class="s-twitter" target="_blank">
+      <a href="https://twitter.com/intent/tweet?url=${url}&text=${title} no '${siteName}'" class="s-twitter" target="_blank">
           <img src="/public/project/icons/twitter.png" alt="Compartilhar no Twitter" />
       </a>
       <a href="https://www.facebook.com/sharer/sharer.php?u=${url}" class="s-facebook" target="_blank">
@@ -27,8 +29,8 @@ module.exports = function(we) {
       <a href="https://www.linkedin.com/shareArticle?mini=true&url=${url}&title={${title}}" class="s-linkedin" target="_blank">
         <img src="/public/project/icons/linkedin.png" alt="Compartilhar no Linkedin" />
       </a>
-      <a href="whatsapp://send?text=${title}: ${url}" data-action="share/whatsapp/share" class="s-whatsapp-mobile"><img src="/public/project/icons/whatsapp.png" alt="Compartilhar no Whatsapp" /></a>
-      <a href="https://web.whatsapp.com/send?text=${title}: ${url}" data-action="share/whatsapp/share" class="s-whatsapp-site" target="_blank"><img src="/public/project/icons/whatsapp.png" alt="Compartilhar no Whatsapp" /></a>
+      <a href="whatsapp://send?text=${title}  no '${siteName}': ${url}" data-action="share/whatsapp/share" class="s-whatsapp-mobile"><img src="/public/project/icons/whatsapp.png" alt="Compartilhar no Whatsapp" /></a>
+      <a href="https://web.whatsapp.com/send?text=${title} no '${siteName}': ${url}" data-action="share/whatsapp/share" class="s-whatsapp-site" target="_blank"><img src="/public/project/icons/whatsapp.png" alt="Compartilhar no Whatsapp" /></a>
       <a href="mailto:?subject=${title}:&amp;body=${title} - Leia mais no link: ${url}"
          title="Compartilhar por Email">
         <img src="/public/project/icons/email.png" alt="Compartilhar por e-mail">
