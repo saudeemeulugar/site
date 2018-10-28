5c2483dad8bfeeaa429f281ccb524a893ff91339
albertosouza <alberto.souza.99@gmail.com>
2018-10-28 17:59:32 -0300

    add my history in user menu and improve history.findOne css

--- plugin.js
+++ plugin.js
@@ -62,6 +62,79 @@ module.exports = function loadPlugin(projectPath, Plugin) {
       'history3': __dirname + '/server/forms/history3.json',
       'history4': __dirname + '/server/forms/history4.json',
       'history5': __dirname + '/server/forms/history5.json'
+    },
+
+    menu: {
+      authenticatedUserMenu(req) {
+        return {
+          links: [
+          {
+            id: 'user',
+            href: '/user/' + req.user.id,
+            text: req.user.displayName || '#'+req.user.id,
+            class: null,
+            weight: 3,
+            dividerAfter: true
+          },
+          {
+            id: 'myHistoryView',
+            href: '/user/' + req.user.id,
+            text: '<i class="glyphicon glyphicon-picture"></i> Minhas histórias',
+            class: null,
+            weight: 2,
+            parent: 'user'
+          },
+          {
+            id: 'userProfileView',
+            href: '/user/' + req.user.id,
+            text: '<i class="glyphicon glyphicon-user"></i> '+
+              req.__('user.profile.view'),
+            class: null,
+            weight: 3,
+            parent: 'user',
+            dividerAfter: true
+          },
+          {
+            id: 'userProfileEdit',
+            href: '/user/' + req.user.id + '/edit',
+            text: '<i class="glyphicon glyphicon-pencil"></i> '+
+              req.__('user.profile.edit'),
+            class: null,
+            weight: 5,
+            parent: 'user'
+          },
+          {
+            id: 'userProfileEditPrivacity',
+            href: '/user/' + req.user.id + '/edit/privacity',
+            text: '<i class="glyphicon glyphicon-alert"></i> '+
+              req.__('Privacity'),
+            class: null,
+            weight: 5,
+            parent: 'user'
+          },
+          {
+            id: 'userChangePassword',
+            href: '/auth/change-password',
+            text: '<i class="glyphicon glyphicon-lock text-warning"></i> '+
+              req.__('auth.change-password'),
+            class: null,
+            weight: 7,
+            parent: 'user',
+            dividerAfter: true
+          },
+          {
+            id: 'logout',
+            href: '/auth/logout',
+            text: '<i class="glyphicon glyphicon-log-out text-danger"></i> '+
+              req.__('Logout'),
+            class: null,
+            weight: 9,
+            parent: 'user',
+            dividerAfter: true
+          }
+          ]
+        };
+      }
     }
   });
 
